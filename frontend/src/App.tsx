693afa98b3af70710494b240d6f928342476287c
PriyanshucRS <priyanshuchouhan@resiliencesoft.com>
2026-03-13 16:15:15 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, type ReactNode } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "./redux/store/store";
 import { Login } from "./pages/Login/Login";
 import { Register } from "./pages/Register/Register";
 import { WatchList } from "./pages/watchlist/Watchlist";
@@ -6,22 +9,29 @@ import { MainLayout } from "./layouts/MainLayout";
 import { Home } from "./pages/Home/Home";
 import { Cart } from "./pages/cart/Cart";
 import { ProductForm } from "./pages/ProductForm/ProductForm";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { fetchCartRequest } from "./redux/Slices/cartSlice";
 import { fetchWatchlistRequest } from "./redux/Slices/watchlistSlice";
-import {fetchProductRequest} from './redux/Slices/productSlice'
+import { fetchProductRequest } from "./redux/Slices/productSlice";
 
+type ProtectedRouteProps = {
+  children: ReactNode;
+};
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  if (!user) return <Navigate to="/login" replace />;
+  return <>{children}</>;
+};
 
 const App = () => {
   const dispatch = useDispatch();
-  const { user } = useSelector((state: any) => state.auth);
+  const { user } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
     if (user) {
       dispatch(fetchCartRequest());
       dispatch(fetchWatchlistRequest());
-      dispatch(fetchProductRequest())
+      dispatch(fetchProductRequest());
     }
   }, [dispatch, user]);
 
@@ -30,9 +40,30 @@ const App = () => {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/watchlist" element={<WatchList />} />
-          <Route path="/cartview" element={<Cart />} />
-          <Route path="/addproduct" element={<ProductForm />} />
+          <Route
+            path="/watchlist"
+            element={
+              <ProtectedRoute>
+                <WatchList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/cartview"
+            element={
+              <ProtectedRoute>
+                <Cart />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/addproduct"
+            element={
+              <ProtectedRoute>
+                <ProductForm />
+              </ProtectedRoute>
+            }
+          />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
